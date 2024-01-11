20f03bd25069dd880962a97bb4808ff4a9fcc025
L0STE <125566964+L0STE@users.noreply.github.com>
2024-01-11 23:24:01 +0100

    Done

--- programs/anchor-address-lookup-table-example/src/lib.rs
+++ programs/anchor-address-lookup-table-example/src/lib.rs
@@ -8,22 +8,28 @@ use solana_address_lookup_table_program::{
     ID as ADDRESS_LOOKUP_TABLE_PROGRAM_ID,
 };
 
-declare_id!("GCxLFEE3BkEUdEnPyWMwZpQWQP9sXSq5wAsq7yR3Rrh");
+declare_id!("ALT4bMa1vKx8fAEbrTYFE1cx86HyKV49LpaNHMBcQZTJ");
 
 #[program]
 pub mod anchor_address_lookup_table_example {
     use super::*;
 
+    /// This function constructs an instruction to create an address lookup table account and returns
+    /// the instruction and the table account's derived address.
     pub fn create_address_lookup_table(ctx: Context<CreateAddressLookupTable>, recent_slot: u64) -> Result<()> {
 
         let (create_table_instruction, table_key) =
         create_lookup_table_signed(
             ctx.accounts.authority.key(),
             ctx.accounts.payer.key(),
-            recent_slot,
+            // ALTs are made with recent_slot as part of the "seeds" because if it can be closed and re-initialized with new addresses, 
+            // any client which is unaware of the change could inadvertently lookup unexpected addresses. To avoid this, all address lookup 
+            // tables must be initialized at an address derived from a recent slot and they cannot be closed until the slot used for 
+            // deactivation is no longer in the slot hashes sysvar.
+            recent_slot, 
         );
 
-        require_keys_eq!(ctx.accounts.lookup_table.key(), table_key); // ADD Error
+        require_keys_eq!(ctx.accounts.lookup_table.key(), table_key, AltError::InvalidAddress);
 
         invoke(
             &create_table_instruction,
@@ -39,7 +45,8 @@ pub mod anchor_address_lookup_table_example {
         Ok(())
     }
     
-    /// Instruction that permanently freeze an address lookup table so that it can never be closed or extended again and making it difatto immutable.
+    /// This function constructs an instruction that permanently freeze an address lookup table so that 
+    /// it can never be closed or extended again and making it immutable.
     /// NB: Empty lookup tables cannot be frozen.
     pub fn freeze_address_lookup_table(ctx: Context<FreezeAddressLookupTable>) -> Result<()> {
 
@@ -57,7 +64,7 @@ pub mod anchor_address_lookup_table_example {
         Ok(())
     }
 
-    /// Instruction that extends an address lookup table account with new addresses.
+    /// This function constructs an instruction that extends an address lookup table account with new addresses.
     /// NB: Funding account and system program account references are only required if the lookup table
     /// account requires additional lamports to cover the rent-exempt balance after being extended.
     pub fn extend_address_lookup_table(ctx: Context<ExtendAddressLookupTable>) -> Result<()> {
@@ -66,7 +73,8 @@ pub mod anchor_address_lookup_table_example {
             ctx.accounts.lookup_table.key(),
             ctx.accounts.authority.key(),
             Some(ctx.accounts.payer.key()),
-            vec![ctx.accounts.publickey_1.key(), ctx.accounts.publickey_2.key(), ctx.accounts.publickey_3.key()], //Max 20
+            // NB: You can add max 20 address at a Time
+            vec![ctx.accounts.publickey_1.key(), ctx.accounts.publickey_2.key(), ctx.accounts.publickey_3.key()], 
         );
 
         invoke(
@@ -83,7 +91,8 @@ pub mod anchor_address_lookup_table_example {
         Ok(())
     }
 
-    /// [...]
+    /// This function constructs an instruction that deactivates an address lookup table so that it cannot be extended again and 
+    /// will be unusable and eligible for closure after a short amount of time.
     pub fn deactivate_address_lookup_table(ctx: Context<DeactivateAddressLookupTable>) -> Result<()> {
 
         let deactivate_table_instruction = deactivate_lookup_table(
@@ -103,6 +112,8 @@ pub mod anchor_address_lookup_table_example {
         Ok(())
     }
 
+    /// This function constructs an instruction that closes an address lookup table account. 
+    /// The account will be deallocated and the lamports will be drained to the recipient address.
     pub fn close_address_lookup_table(ctx: Context<CloseAddressLookupTable>) -> Result<()> {
 
         let close_lookup_table_instruction = close_lookup_table(
@@ -166,11 +177,11 @@ pub struct ExtendAddressLookupTable<'info> {
     /// CHECK: the account will be validated by the lookup table program
     pub lookup_table: AccountInfo<'info>,
 
-    /// CHECK: We don't care
+    /// CHECK: We don't care to check it since we're just putting it in the lookup table
     pub publickey_1: AccountInfo<'info>,
-    /// CHECK: We don't care
+    /// CHECK: We don't care to check it since we're just putting it in the lookup table
     pub publickey_2: AccountInfo<'info>,
-    /// CHECK: We don't care
+    /// CHECK: We don't care to check it since we're just putting it in the lookup table
     pub publickey_3: AccountInfo<'info>,
 
     #[account(address = ADDRESS_LOOKUP_TABLE_PROGRAM_ID)]
@@ -202,11 +213,17 @@ pub struct CloseAddressLookupTable<'info> {
     pub lookup_table: AccountInfo<'info>,
 
     #[account(mut)]
-    /// CHECK: we don't care
+    /// CHECK: We don't care to check it since it's just getting the funds from the closure of the ALT
     pub recipient: AccountInfo<'info>,
 
     #[account(address = ADDRESS_LOOKUP_TABLE_PROGRAM_ID)]
     /// CHECK: the account will be validated by the lookup table program
     pub address_lookup_table_program: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum AltError {
+    #[msg("The address lookup table passed in as account doesn't match the one derived in the program")]
+    InvalidAddress,
 }
